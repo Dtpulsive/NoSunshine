e22842a9df7b453f52f52a745d1dee293850536f
Dtpulsive <75098390+Dtpulsive@users.noreply.github.com>
2026-09-23 11:53:12 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,45 @@
+public class NoSunshine {
+public static void main(String[] args) { 
+gone();
+System.out.println("It's not warm when she's away");
+gone();
+System.out.println("And she's always gone too long");
+away();
+System.out.println("\nWonder this time where she's gone\nWonder if she's gone to stay");
+gone();
+System.out.println("And this house just ain't no home");
+away();
+System.out.print("And ");
+k(4);
+System.out.println();
+k(5);
+System.out.println();
+k(5);
+System.out.println();
+k(5);
+System.out.println();
+k(5);
+System.out.println();
+k(2);
+System.out.println("\n\nHey, I ought to leave young thing alone\nBut aint't no sunshine when she's gone, whoa-whoa");
+gone();
+System.out.println("Only darkness every day\n");
+gone();
+System.out.println("And this house just ain't no home\n");
+away();
+away();
+away();
+away();
+}  
+public static void gone() {
+System.out.println("Ain't no sunshine when she's gone");
+}
+public static void away() {
+System.out.println("Anytime she goes away");
+}
+public static void k(int n) {
+for (int g = 0; g < n; g++) { 
+System.out.print("I know, ");
+}
+}
+}
